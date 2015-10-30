3e3451d450124cba9c3b6188b6b8ba5fd5e17464
Juan Manuel <jmanuel816@gmail.com>
2015-10-29 20:28:36 -0500

    En este commit se sube: 	1) El segundo formulario muy adelantado

--- main/templates/static/js/scripts.js
+++ main/templates/static/js/scripts.js
@@ -1,47 +1,97 @@
-window.onload= function(){cambiarCiudades("CO");}
+var dropdownsCiudades = ["ciudad_datos", "ciudad_origen", "ciudad_destino"];
+var iconoCargando = ["icono_datos", "icono_origen", "icono_destino"];
 
+window.onload = function()
+{
+	mostrarPag(1);
+	for(var i=0; i<dropdownsCiudades.length; i++) cambiarCiudades("IO", i);
+}
+
+//Este metodo se llama desde los botones de siguiente y anterior
 function mostrarPag(pagina)
 {
 	if(pagina==1)
 	{
-		document.getElementById("bola1").setAttribute("class", "bolitas primera activa movidas");
-		document.getElementById("bola2").setAttribute("class", "bolitas segunda apagada movidas");
-		document.getElementById("bola3").setAttribute("class", "bolitas tercera apagada");
+		//Sobre aparecer las diferentes partes del formulario
+		$("#Parte1").css("display", "block");
+		$("#Parte2").css("display", "none");
+		$("#Parte3").css("display", "none");
+		//Sobre colorear las bolitas
+		$("#bola1").attr("class", "bolitas primera activa movidas");
+		$("#bola2").attr("class", "bolitas segunda apagada movidas");
+		$("#bola3").attr("class", "bolitas tercera apagada");
+		//Sobre los colores de los textos bajo las bolas
+		$("#textoB1").css("color", "#000000");
+		$("#textoB2").css("color", "#909090");
+		$("#textoB3").css("color", "#909090");
 	}
 	else if(pagina==2)
 	{
-		document.getElementById("bola1").setAttribute("class", "bolitas primera apagada movidas");
-		document.getElementById("bola2").setAttribute("class", "bolitas segunda activa movidas");
-		document.getElementById("bola3").setAttribute("class", "bolitas tercera apagada");
+		//Sobre aparecer las diferentes partes del formulario
+		$("#Parte1").css("display", "none");
+		$("#Parte2").css("display", "block");
+		$("#Parte3").css("display", "none");
+		//Sobre colorear las bolitas
+		$("#bola1").attr("class", "bolitas primera apagada movidas");
+		$("#bola2").attr("class", "bolitas segunda activa movidas");
+		$("#bola3").attr("class", "bolitas tercera apagada");
+		//Sobre los colores de los textos bajo las bolas
+		$("#textoB1").css("color", "#909090");
+		$("#textoB2").css("color", "#000000");
+		$("#textoB3").css("color", "#909090");
 	}
 	else if(pagina==3)
 	{
+		//Sobre aparecer las diferentes partes del formulario
+		$("#Parte1").css("display", "none");
 		$("#Parte2").css("display", "none");
-		document.getElementById("bola1").setAttribute("class", "bolitas primera apagada movidas");
-		document.getElementById("bola2").setAttribute("class", "bolitas segunda apagada movidas");
-		document.getElementById("bola3").setAttribute("class", "bolitas tercera activa");
+		$("#Parte3").css("display", "block");
+		//Sobre colorear las bolitas
+		$("#bola1").attr("class", "bolitas primera apagada movidas");
+		$("#bola2").attr("class", "bolitas segunda apagada movidas");
+		$("#bola3").attr("class", "bolitas tercera activa");
+		//Sobre los colores de los textos bajo las bolas
+		$("#textoB1").css("color", "#909090");
+		$("#textoB2").css("color", "#909090");
+		$("#textoB3").css("color", "#000000");
 	}
 }
 
-//Aqui se esta usando ajax de una forma mas sencilla pero que 
-function cambiarCiudades(cc_fips)
+//Aqui se esta usando ajax de una forma sencilla, como se explica en w3school
+function cambiarCiudades(cc_fips, target)
 {
-	var xhttp;    
+	var idDropdown = "#"+dropdownsCiudades[target];
+	var idIcono = "#"+iconoCargando[target];
+
 	if(cc_fips == "")
 	{
-		document.getElementById("ciudades").innerHTML = "";
+		$(idDropdown).html("");
 		return;
 	}
-	xhttp = new XMLHttpRequest();
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function()
 	{
-		if (xhttp.readyState == 4 && xhttp.status == 200)
+		if(xhttp.readyState==4 && xhttp.status==200)
 		{
-			document.getElementById("ciudades").innerHTML = xhttp.responseText;
-			document.getElementById("dePrueba").setAttribute("src", "");
+			$(idDropdown).html(xhttp.responseText);
+			$(idIcono).attr("src", "");
 		}
 	}
 	xhttp.open("GET", "auxiliar/get/"+cc_fips, true);
+	$(idIcono).attr("src", "/static/img/loading.gif");
 	xhttp.send();
-	document.getElementById("dePrueba").setAttribute("src", "http://www.geeksphone.com/wp-content/themes/gp/img/loading.gif");
 }
+
+//Cosas relacionadas con Angular
+var app = angular.module('appMelyak', []);
+app.controller('ctrlMelyak', function($scope){
+	$scope.update = function(user) {
+		$scope.master = angular.copy(user);
+	};
+
+	$scope.reset = function() {
+		$scope.user = angular.copy($scope.master);
+	};
+
+	$scope.reset();
+});
